f701d2c046e326a27b99b2a582707b3c01002689
shiva20991 <80451254+shiva20991@users.noreply.github.com>
2021-06-23 19:36:07 +0530

    Add files via upload

--- plugins/gofiles.py
+++ plugins/gofiles.py
@@ -65,7 +65,7 @@ async def query_mgs(client: Bot, message: Message):
                                 chat_id=message.from_user.id,
                                 from_chat_id=messages.chat.id,
                                 message_id=messages.message_id,
-                                caption=Config.GROUP_U_NAME+Presets.CAPTION_TEXT_DOC.format(
+                                caption=Config.GROUP_U_NAME+Presets.CAPTION_TEXT_DOC.format(media_name,
                                                                                             media_format, file_size)
                             )
                         except FloodWait as e:
